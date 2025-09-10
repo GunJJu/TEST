99f4ce2007a91e9bf7fbfff6ff57c80d89faa2b6
GunJJu <smkim0721@gmail.com>
2025-09-10 14:31:14 +0900

    내용쓰고

---
+++ TEST.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class TEST {
+    public static void main(String[] args) {
+        int num = 14;
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("안녕하세요");
+    }
+}
